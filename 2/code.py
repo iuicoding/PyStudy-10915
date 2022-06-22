86bed00ff020c78e6f3137eaf66c233927292422
yeonjooo <yjoolee7718@gmail.com>
2022-06-22 15:35:51 +0900

    update

--- 2/code.py
+++ 2/code.py
@@ -11,3 +11,35 @@ elif 1 <= score:
      print("사람이야?")
 else:
      print("9등급 인생 포기해")
+
+
+
+
+#6교시 수업내용
+alist = [1,2,3,4,5]
+
+print(alist)
+
+if 4 in list:
+     print("O")
+if 144 not in alist:
+     print("X")
+
+print(alist[1])
+
+alist = alist + [1]
+alist = alist * 2
+alist.append(8)
+print(alist)
+
+del alist[0]
+alist.remove(2)
+alist.pop(4)
+print(alist)
+
+print(alist + [2])
+print(alist * 3)
+
+for element in a:
+     print(element)
+
